6fee687487f9173f3b2383c9798e53751ba8085f
Tom Hinton <tomjhinton@gmail.com>
2019-12-28 21:40:54 +0000

    map on page

---
+++ src/components/main/Main.js
@@ -0,0 +1,63 @@
+//pic size 687*687
+import React from 'react'
+import axios from 'axios'
+import {Link} from 'react-router-dom'
+import ReactMapboxGl, { Layer, Feature, Popup, Marker } from 'react-mapbox-gl'
+
+const Map = ReactMapboxGl({
+  accessToken:
+    process.env.mapboxPublicToken
+})
+
+
+class Main extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      data: {},
+      error: ''
+
+    }
+    this.componentDidMount = this.componentDidMount.bind(this)
+  }
+
+
+  componentDidMount(){
+    // axios.get('/api/records')
+    //   .then(res => this.setState({ records: res.data }))
+
+
+  }
+
+  render() {
+
+    console.log(this.state)
+
+    return (
+      <div className='container'>
+
+        <Map
+          style="mapbox://styles/mapbox/streets-v9"
+          containerStyle={{
+            height: '100vh',
+            width: '100vw'
+          }}
+        >
+          <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+            <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+          </Layer>
+          <Popup coordinates={[-0.481747846041145, 51.3233379650232]}>
+          HIYa
+          </Popup>
+        </Map>
+
+
+      </div>
+
+
+
+
+    )
+  }
+}
+export default Main
